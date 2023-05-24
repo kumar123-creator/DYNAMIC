b127d3ad143e3f582b7bcc9dabec646dad66d981
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-24 10:25:51 +0530

    code updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,150 +1,99 @@
 <script>
-
-    let fullName = '';
-
-    let email = '';
-
-    let mobile = '';
-
-    let cvFile = null;
-
-    let fileContent = '';
-
-    let fileName = '';
-
- 
-
-    async function handleFileChange(event) {
-
-      cvFile = event.target.files[0];
-
-      fileContent = await toBase64(cvFile);
-
-      fileName = cvFile.name;
-
-    }
-
- 
-
-    async function uploadCV() {
-
-      if (cvFile && fileContent && fileName) {
-
-        const postData = {
-
-          fullName,
-
-          email,
-
-          mobile,
-
-          fileContent,
-
-          fileName
-
-        };
-
- 
-
-        try {
-
-          const response = await fetch('https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
-
-            method: 'POST',
-
-            headers: {
-
-              'Content-Type': 'application/json'
-
-            },
-
-            body: JSON.stringify(postData)
-
-          });
-
- 
-
-          // Handle the response as needed
-
-          if (response.ok) {
-
-            console.log('CV uploaded successfully');
-
-          } else {
-
-            console.error('Failed to upload CV');
-
-          }
-
-        } catch (error) {
-
-          console.error('Error uploading CV:', error);
-
-        }
-
-      }
-
-    }
-
- 
-
-    function toBase64(file) {
-
-      return new Promise((resolve, reject) => {
-
-        const reader = new FileReader();
-
-        reader.readAsDataURL(file);
-
-        reader.onload = () => resolve(reader.result.split(',')[1]);
-
-        reader.onerror = error => reject(error);
-
-      });
-
-    }
-
-  </script>
-
- 
-
-  <main>
-
-    <h1>Upload CV</h1>
-
-    <label>
-
-      Full Name:
-
-      <input type="text" bind:value={fullName} />
-
-    </label>
-
-    <label>
-
-      Email:
-
-      <input type="email" bind:value={email} />
-
-    </label>
-
-    <label>
-
-      Mobile:
-
-      <input type="tel" bind:value={mobile} />
-
-    </label>
-
-    <input type="file" accept=".pdf,.doc,.docx" on:change={handleFileChange} />
-
-    <button on:click={uploadCV}>Upload</button>
-
-  </main>
-
- 
-
-
-
-
-
+  import 'bootstrap/dist/css/bootstrap.min.css';
+
+  let fullName = '';
+  let email = '';
+  let mobile = '';
+  let cvFile = null;
+  let fileContent = '';
+  let fileName = '';
+
+  async function handleFileChange(event) {
+    cvFile = event.target.files[0];
+    fileContent = await toBase64(cvFile);
+    fileName = cvFile.name;
+  }
+
+  async function uploadCV() {
+    if (cvFile && fileContent && fileName) {
+      const postData = {
+        fullName,
+        email,
+        mobile,
+        fileContent,
+        fileName
+      };
+
+      try {
+        const response = await fetch(
+          'https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+          }
+        );
+
+        // Handle the response as needed
+        if (response.ok) {
+          console.log('CV uploaded successfully');
+        } else {
+          console.error('Failed to upload CV');
+        }
+      } catch (error) {
+        console.error('Error uploading CV:', error);
+      }
+    }
+  }
+
+  function toBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result.split(',')[1]);
+      reader.onerror = error => reject(error);
+    });
+  }
+
+  function resetForm() {
+    fullName = '';
+    email = '';
+    mobile = '';
+    cvFile = null;
+    fileContent = '';
+    fileName = '';
+    document.getElementById('cv-upload').value = null;
+  }
+</script>
+
+<main>
+  <h1>Upload CV</h1>
+
+  <label>
+    Full Name:
+    <input type="text" class="form-control" bind:value={fullName} />
+  </label>
+
+  <label>
+    Email:
+    <input type="email" class="form-control" bind:value={email} />
+  </label>
+
+  <label>
+    Mobile:
+    <input type="tel" class="form-control" bind:value={mobile} />
+  </label>
+
+  <input
+    id="cv-upload"
+    type="file"
+    accept=".pdf,.doc,.docx"
+    class="form-control-file"
+    on:change={handleFileChange}
+  />
+
+  <button class="btn btn-primary" on:click={uploadCV}>Upload</button>
+  <button class="btn btn-secondary" on:click={resetForm}>Reset</button>
+</main>
