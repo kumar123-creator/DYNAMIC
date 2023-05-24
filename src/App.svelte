af27838ad2daa2109ec68cc6520d0ce852916f36
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-24 10:17:14 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,131 +1,150 @@
 <script>
-  import 'bootstrap/dist/css/bootstrap.min.css';
 
-  let formData = {
-    name: '',
-    email: '',
-    mobile: ''
-  };
-  
-  let cvFile = null;
+    let fullName = '';
 
-  function handleFileChange(event) {
-    cvFile = event.target.files[0];
-  }
+    let email = '';
 
-  async function uploadCV() {
-    if (cvFile) {
-      const base64Data = await toBase64(cvFile);
-      const postData = {
-        fileContent: base64Data
-      };
+    let mobile = '';
 
-      const response = await fetch('https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postData)
-      });
+    let cvFile = null;
 
-      // Handle the response as needed
-    }
-  }
+    let fileContent = '';
 
-  function toBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result.split(',')[1]);
-      reader.onerror = error => reject(error);
-    });
-  }
+    let fileName = '';
 
-  function handleSubmit() {
-    const jsonData = {
-      ...formData,
-      cv: cvFile
-    };
+ 
 
-    const formData = new FormData();
-    formData.append('data', JSON.stringify(jsonData));
-    formData.append('cv', cvFile);
+    async function handleFileChange(event) {
 
-    fetch('https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
-      method: 'POST',
-      body: formData
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response data
-        console.log(data);
-      })
-      .catch(error => {
-        // Handle any errors
-        console.error(error);
-      });
-  }
+      cvFile = event.target.files[0];
 
-  function handleCVUpload(event) {
-    cvFile = event.target.files[0];
-  }
-</script>
+      fileContent = await toBase64(cvFile);
 
-<main>
-  <form on:submit={handleSubmit}>
-    <div class="form-group">
-      <label for="name">Name</label>
-      <input type="text" id="name" name="name" class="form-control" bind:value={formData.name} />
-    </div>
+      fileName = cvFile.name;
 
-    <div class="form-group">
-      <label for="email">Email</label>
-      <input type="email" id="email" name="email" class="form-control" bind:value={formData.email} />
-    </div>
+    }
 
-    <div class="form-group">
-      <label for="mobile">Mobile</label>
-      <input type="tel" id="Mobile" name="mobile" class="form-control" bind:value={formData.mobile} />
-    </div>
+ 
 
-    <div class="form-group">
-      <label for="cv">Upload CV</label>
-      <input type="file" id="cv" name="cv" class="form-control-file" on:change={handleCVUpload} />
-    </div>
+    async function uploadCV() {
 
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</main>
+      if (cvFile && fileContent && fileName) {
 
-<style>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
+        const postData = {
 
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 300px;
-  }
+          fullName,
 
-  label {
-    margin-bottom: 0.5rem;
-  }
+          email,
 
-  input {
-    margin-bottom: 1rem;
-  }
+          mobile,
 
-  .form-group {
-    margin-bottom: 1.5rem;
-  }
+          fileContent,
 
-  .btn {
-    padding: 0.5rem 1rem;
-  }
-</style>
-``
+          fileName
+
+        };
+
+ 
+
+        try {
+
+          const response = await fetch('https://api.recruitly.io/api/cvsubmit/bytes?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
+
+            method: 'POST',
+
+            headers: {
+
+              'Content-Type': 'application/json'
+
+            },
+
+            body: JSON.stringify(postData)
+
+          });
+
+ 
+
+          // Handle the response as needed
+
+          if (response.ok) {
+
+            console.log('CV uploaded successfully');
+
+          } else {
+
+            console.error('Failed to upload CV');
+
+          }
+
+        } catch (error) {
+
+          console.error('Error uploading CV:', error);
+
+        }
+
+      }
+
+    }
+
+ 
+
+    function toBase64(file) {
+
+      return new Promise((resolve, reject) => {
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = () => resolve(reader.result.split(',')[1]);
+
+        reader.onerror = error => reject(error);
+
+      });
+
+    }
+
+  </script>
+
+ 
+
+  <main>
+
+    <h1>Upload CV</h1>
+
+    <label>
+
+      Full Name:
+
+      <input type="text" bind:value={fullName} />
+
+    </label>
+
+    <label>
+
+      Email:
+
+      <input type="email" bind:value={email} />
+
+    </label>
+
+    <label>
+
+      Mobile:
+
+      <input type="tel" bind:value={mobile} />
+
+    </label>
+
+    <input type="file" accept=".pdf,.doc,.docx" on:change={handleFileChange} />
+
+    <button on:click={uploadCV}>Upload</button>
+
+  </main>
+
+ 
+
+
+
+
+
